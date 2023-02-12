b1a08cd4dd2f42ec34bcf4179dd103e21dbc5f1d
Talsumi <92010474+Talsumi@users.noreply.github.com>
2023-02-13 09:08:43 +1100

    Changed license to GPL2

--- src/main/kotlin/talsumi/statuesclassic/client/content/render/entity/StatuePlayerRenderer.kt
+++ src/main/kotlin/talsumi/statuesclassic/client/content/render/entity/StatuePlayerRenderer.kt
@@ -1,27 +1,3 @@
-/*
- * MIT License
- *
- * Copyright (c) 2022 Talsumi
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package talsumi.statuesclassic.client.content.render.entity
 
 import net.minecraft.client.MinecraftClient
@@ -53,7 +29,6 @@ import net.minecraft.util.Arm
 import net.minecraft.util.math.Vec3f
 import net.minecraft.world.RaycastContext
 import talsumi.statuesclassic.client.compat.SkippedFeatureRenderers
-import talsumi.statuesclassic.client.content.entity.StatuePlayerEntity
 import talsumi.statuesclassic.client.core.StatueModels
 import talsumi.statuesclassic.content.blockentity.StatueBE
 import talsumi.statuesclassic.core.StatueData
